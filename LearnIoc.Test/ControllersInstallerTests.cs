5d9d3926dfc8a53efedfcadb380bef6f611a55da
陈亮 <cl889521@163.com>
2016-11-21 22:15:23 +0800

    正在学习测试

---
+++ LearnIoc.Test/ControllersInstallerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Castle.Windsor;
+
+namespace LearnIoc.Test
+{
+    [TestClass]
+    public class ControllersInstallerTests
+    {
+        private IWindsorContainer containerWithControllers;
+
+        public ControllersInstallerTests()
+        {
+            containerWithControllers=new WindsorContainer().Install(new )
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
