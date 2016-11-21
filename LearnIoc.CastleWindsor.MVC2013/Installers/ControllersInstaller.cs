c3ae14c79dbb709dcd2504f834a6ac9f827327d6
taohonggou <cl889521@163.com>
2016-11-21 19:04:26 +0800

    开始学习part three

---
+++ LearnIoc.CastleWindsor.MVC2013/Installers/ControllersInstaller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnIoc.CastleWindsor.MVC2013.Installers
+{
+    public class ControllersInstaller
+    {
+    }
+}
